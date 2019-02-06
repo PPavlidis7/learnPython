af7a77b7951f612c4354324f3ca5dceeb0d4729b
PavlidisPavlos <dai16035@uom.edu.gr>
2019-02-06 20:36:21 +0200

    my first commit

---
+++ Count_Sort/Serial.py
@@ -0,0 +1,40 @@
+import sys
+
+N = 7
+
+
+def usage(prog_name):
+    sys.exit('usage:' + prog_name + '<files name> <thread_count>')
+
+
+def readfile():
+    # file = sys.argv[1];
+    file = '../numbers.txt'
+    files_numbers = []
+    with open(file, 'r') as f:
+        lines = f.readlines()
+        for item in lines:
+            files_numbers.append(int(item.split()[0]))
+        return files_numbers
+
+
+def count_sort_ser(numbers):
+    temp = []
+    for i in range(0, N):
+        count = 0
+        for j in range(0, N):
+            if numbers[j] < numbers[i]:
+                count += 1
+            elif numbers[j] == numbers[i] & j < i:
+                count += 1
+        temp[count] = numbers[i]
+    return temp
+
+
+if __name__ == '__main__':
+    # if len(sys.argv) != 3:
+    #     Usage(sys.argv[0])
+
+    numbers = readfile()
+    final = count_sort_ser(numbers)
+    print(final)
