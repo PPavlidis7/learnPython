ec955d47eed1fc016428583cb081d48bf2b7029d
pavlidis-4@hotmail.com <pavlidis-2009>
2019-03-31 16:28:13 +0300

    Added csr with multiprocessing

--- ptyxiaki/CSR.py
+++ ptyxiaki/CSR.py
@@ -1,9 +1,11 @@
-import  numpy as np
+import multiprocessing as mp
+
+import numpy as np
 
 
 def read_matrix():
     # A = []
-    with open('output.txt', 'r') as f:
+    with open('matrix.txt', 'r') as f:
         A = [list(map(int, line.split())) for line in f]
     return A
 
@@ -24,7 +26,41 @@ def create_vectors():
     print("IA = ", IA)
     print("JA = ", JA)
 
-    return AR,IA,JA
+    return AR, IA, JA
+
+
+def create_vectors_paral(line, AR, priv_IA, priv_JA):
+    ne_coutner = 0
+    for index, value in enumerate(line):
+        if value != 0:
+            ne_coutner += 1
+            AR.append(value)
+            priv_JA.append(index)
+
+    priv_IA.value = ne_coutner
+
+
+def initialize_paral():
+    print("----------------------------------")
+    A = read_matrix()
+    IA, processes = [], []
+    IA.append(1)
+    ne_counter = 1
+
+    manager = mp.Manager()
+    AR = manager.list()
+    JA = manager.list()
+    for row, line in enumerate(A):
+        last_IA = IA[len(IA) - 1]
+        priv_IA = mp.Value('i', 0)
+        proccess = mp.Process(target=create_vectors_paral, args=(line, AR, priv_IA, JA))
+        proccess.start()
+        proccess.join()
+
+        IA = IA + [priv_IA.value + last_IA]
+    print("AR = ", AR)
+    print("IA = ", IA)
+    print("JA = ", JA)
 
 
 def take_it_back(AR,IA,JA):
@@ -50,4 +86,5 @@ def take_it_back(AR,IA,JA):
 
 if __name__ == '__main__':
     AR,IA,JA=create_vectors()
-    take_it_back(AR,IA,JA)
+    # take_it_back(AR,IA,JA)
+    initialize_paral()
