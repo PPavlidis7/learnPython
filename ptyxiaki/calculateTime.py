5232e4f287daa71bc3dc93226c496d52310c07a9
pavlos <pavlidis-4@hotmail.com>
2019-05-26 18:24:00 +0300

    changed script for average time

--- ptyxiaki/calculateTime.py
+++ ptyxiaki/calculateTime.py
@@ -1,31 +1,88 @@
+def create_dicts():
+    time_dict = {
+        'CSR': {
+            '5000': 0,'7000': 0, '9000': 0, '11000': 0, '13000': 0, '15000': 0,
+            '17000': 0, '19000': 0, '21000': 0
+        },
+        'CSC': {
+            '5000': 0,'7000': 0, '9000': 0, '11000': 0, '13000': 0, '15000': 0,
+            '17000': 0, '19000': 0, '21000': 0
+        },
+        'COO': {
+            '5000': 0,'7000': 0, '9000': 0, '11000': 0, '13000': 0, '15000': 0,
+            '17000': 0, '19000': 0, '21000': 0
+        },
+        'CSR3': {
+            '5000': 0,'7000': 0, '9000': 0, '11000': 0, '13000': 0, '15000': 0,
+            '17000': 0, '19000': 0, '21000': 0
+        },
+        'COO3': {
+            '5000': 0,'7000': 0, '9000': 0, '11000': 0, '13000': 0, '15000': 0,
+            '17000': 0, '19000': 0, '21000': 0
+        },
+        'Diagonal': {
+            '5000': 0,'7000': 0, '9000': 0, '11000': 0, '13000': 0, '15000': 0
+        },
+    }
+
+    read_time_dict = {
+        'comprese_v2': {
+            '5000': 0, '7000': 0, '9000': 0, '11000': 0, '13000': 0, '15000': 0,
+            '17000': 0, '19000': 0, '21000': 0
+        },
+        'comprese_v3': {
+            '5000': 0, '7000': 0, '9000': 0, '11000': 0, '13000': 0, '15000': 0,
+            '17000': 0, '19000': 0, '21000': 0
+        },
+    }
+    return time_dict, read_time_dict
 
 
-def read_times():
+def read_exec_times():
     times = []
-    with  open('./execution_time.txt', 'r') as f:
+    with open('./execution_time.txt', 'r') as f:
         for lines in f:
             times.append(lines.split())
     return times
 
-def calculate_time(times):
-    time_dict = {}
+
+def read_read_times():
+    times = []
+    with open('./execution_time.txt', 'r') as f:
+        for lines in f:
+            times.append(lines.split())
+    return times
+
+
+def calculate_time(times,time_dict,type):
     for time in times:
-        time_dict[time[0]] = 0.0
-    for time in times:
-        time_dict[time[0]] += float(time[1])
+        time_dict[time[0]][time[1]] += float(time[2])
+
     for item in time_dict:
-        time_dict[item] = time_dict[item]/5.0
-    print(time_dict)
-    write_times(time_dict,times)
+        for inner_item in time_dict[item]:
+            if item == 'Diagonal':
+                time_dict[item][inner_item] = time_dict[item][inner_item]/3.0
+            else:
+                time_dict[item][inner_item] = time_dict[item][inner_item]/5.0
+
+    # print(time_dict)
+    write_times(time_dict,type)
 
 
-def write_times(t_dict,times):
-    with open('CRS_time.txt', 'w') as f:
-        for key,value in t_dict.items():
-            f.write("%s\t%s\n" % (key,value))
+def write_times(t_dict, type):
+    file_name = 'exec_times_final.txt'
+    if type == 'read':
+        file_name = 'read_times_final.txt'
+
+    with open(file_name, 'w') as f:
+        for key, inner_dict in t_dict.items():
+            for key_inner, value in inner_dict.items():
+                f.write("%s\t%s\t%s\n" % (key, key_inner, value))
 
 
 if __name__ == '__main__':
-    times = read_times()
-    calculate_time(times)
-    # print(times)
+    exec_time, read_time = create_dicts()
+    exec_times_file = read_exec_times()
+    calculate_time(exec_times_file,exec_time, 'exec')
+    read_times_file = read_read_times()
+    calculate_time(read_times_file, read_time, 'write')
